56afd143ea2b00e5dd04ce50644f581596b2c254
elyseq <equigley@macalester.edu>
2026-04-15 20:08:58 -0500

    Initial Commit

---
+++ DailyJournal/DailyJournalApp.swift
@@ -0,0 +1,32 @@
+//
+//  DailyJournalApp.swift
+//  DailyJournal
+//
+//  Created by Elyse Q on 4/15/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DailyJournalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DailyJournalTests/DailyJournalTests.swift
@@ -0,0 +1,18 @@
+//
+//  DailyJournalTests.swift
+//  DailyJournalTests
+//
+//  Created by Elyse Q on 4/15/26.
+//
+
+import Testing
+
+struct DailyJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
